80ff0990f056a6f8d812f762f94ef0ec083b2a50
Peter Duggins <psipeter@hydra.dartmouth.edu>
2025-06-04 15:25:16 -0400

    move v_chosen calculation inside the network

--- model_0.py
+++ model_0.py
@@ -9,7 +9,7 @@ import sys
 class Environment():
     def __init__(self, sid, seed=1, t_load=1, t_cue=0.5, t_reward=0.5, dt=0.001,
                  alpha_plus=0.47, alpha_minus=0.44, gamma_u=0.32,
-                 omega_0=0.35, alpha_omega=0.31, gamma_omega=0.09, reward_schedule=0.7):
+                 omega_0=0.35, alpha_omega=0.31, gamma_omega=0.09, p_reward=0.7):
         self.empirical = pd.read_pickle("data/empirical.pkl")
         self.sid = sid
         self.rng = np.random.RandomState(seed=seed)
@@ -23,7 +23,7 @@ class Environment():
         self.omega_0 = omega_0
         self.alpha_omega = alpha_omega
         self.gamma_omega = gamma_omega
-        self.reward_schedule = reward_schedule
+        self.p_reward = p_reward
         self.letter  = [0,0]  # [A=-1, B=1]
         self.location = [0,0]  # [left=-1, right=1]
         self.omega = omega_0    # 0 to 1; 0.5 is equal arbitration
@@ -31,7 +31,6 @@ class Environment():
         self.alpha = [0,0]
         self.gamma = [0,0]
         self.action = [0,0]
-        self.v_chosen = [0,0]
         self.feedback_phase = 0
         self.cue_phase = 1
     def set_cue(self, bid, trial):
@@ -44,26 +43,18 @@ class Environment():
         self.alpha = [0,0]
         self.gamma = [0,0]
         self.action = [1,1]  # prevents learning during cue phase
-        self.v_chosen = [0,0]
         self.cue_phase = 1
         self.feedback_phase = 0
-    # def set_reward_schedule(self):
-    #     self.reward_schedule = [0.8, 0.7, 0.6][self.rng.randint(3)]
     def set_action(self, sim, net):
         act = sim.data[net.p_action][-1]
         self.action = [1, 0] if act[0]>act[1] else [0,1]
-    def set_omega(self, sim, net):
-        delta_reliability = self.v_chosen[0] - self.v_chosen[1]
-        if delta_reliability > 0:
-            self.omega += delta_reliability * self.alpha_omega * (1 - self.omega)  + self.gamma_omega * (self.omega_0 - self.omega)
-        else:
-            self.omega += np.abs(delta_reliability) * self.alpha_omega * (0 - self.omega) + self.gamma_omega * (self.omega_0 - self.omega)
     def set_reward(self, bid, trial):
+        sid = self.sid
         block = self.empirical.query("sid==@sid & bid==@bid & trial==@trial")['block'].to_numpy()[0]
         correct = self.empirical.query("sid==@sid & bid==@bid & trial==@trial")['correct'].to_numpy()[0]
         if self.action==[1,0]:
             if correct=='left':
-                if self.rng.uniform(0,1) < self.reward_schedule:    # yes rewarded for picking the better option
+                if self.rng.uniform(0,1) < self.p_reward:    # yes rewarded for picking the better option
                     self.reward = [1,0]
                     self.alpha = [self.alpha_plus,0]
                     self.gamma = [0,self.gamma_u]
@@ -72,7 +63,7 @@ class Environment():
                     self.alpha = [self.alpha_minus,0]
                     self.gamma = [0,self.gamma_u]
             else:
-                if self.rng.uniform(0,1) < 1-self.reward_schedule:  # yes rewarded for picking the worse option 
+                if self.rng.uniform(0,1) < 1-self.p_reward:  # yes rewarded for picking the worse option 
                     self.reward = [1,0]
                     self.alpha = [self.alpha_plus,0]
                     self.gamma = [0,self.gamma_u]
@@ -82,7 +73,7 @@ class Environment():
                     self.gamma = [0,self.gamma_u]
         else:
             if correct=='right':
-                if self.rng.uniform(0,1) < self.reward_schedule:    # yes rewarded for picking the better option
+                if self.rng.uniform(0,1) < self.p_reward:    # yes rewarded for picking the better option
                     self.reward = [0,1]
                     self.alpha = [0, self.alpha_plus]
                     self.gamma = [self.gamma_u,0]
@@ -91,7 +82,7 @@ class Environment():
                     self.alpha = [0, self.alpha_minus]
                     self.gamma = [self.gamma_u,0]
             else:
-                if self.rng.uniform(0,1) < 1-self.reward_schedule:  # yes rewarded for picking the worse option
+                if self.rng.uniform(0,1) < 1-self.p_reward:  # yes rewarded for picking the worse option
                     self.reward = [0,1] 
                     self.alpha = [0, self.alpha_plus]
                     self.gamma = [self.gamma_u,0]
@@ -101,15 +92,21 @@ class Environment():
                     self.gamma = [self.gamma_u,0]
         self.cue_phase = 0
         self.feedback_phase = 1
-    def set_v_chosen(self, sim, net):
+    def set_omega(self, sim, net):
         chosen = 0 if self.action==[1,0] else 1
         v_c_letter = sim.data[net.p_value_letter][-1,chosen]
         v_c_location = sim.data[net.p_value_location][-1,chosen]
-        self.v_chosen = [v_c_letter, v_c_location]
+        delta_reliability = v_c_letter - v_c_location
+        if delta_reliability > 0:
+            self.omega += delta_reliability * self.alpha_omega * (1 - self.omega)  + self.gamma_omega * (self.omega_0 - self.omega)
+        else:
+            self.omega += np.abs(delta_reliability) * self.alpha_omega * (0 - self.omega) + self.gamma_omega * (self.omega_0 - self.omega)
     def sample_letter(self, t):
         return self.letter
     def sample_location(self, t):
         return self.location
+    def sample_action(self, t):
+        return self.action
     def sample_omega(self, t):
         return self.omega
     def sample_reward(self, t):
@@ -118,14 +115,8 @@ class Environment():
         return self.alpha
     def sample_gamma(self, t):
         return self.gamma
-    def sample_action(self, t):
-        return self.action
-    def sample_unchosen(self, t):
-        return [1-self.action[0], 1-self.action[1]]
     def sample_phase(self, t):
         return [self.cue_phase, self.feedback_phase]
-    def sample_v_chosen(self, t):
-        return [self.v_chosen[0], self.v_chosen[1]]
 
 def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
     net = nengo.Network(seed=seed_network)
@@ -140,12 +131,11 @@ def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
         in_letter = nengo.Node(lambda t: env.sample_letter(t))
         in_location = nengo.Node(lambda t: env.sample_location(t))
         in_omega = nengo.Node(lambda t: env.omega_0)
+        in_action = nengo.Node(lambda t: env.sample_action(t))
         in_reward = nengo.Node(lambda t: env.sample_reward(t))
         in_alpha = nengo.Node(lambda t: env.sample_alpha(t))
         in_gamma = nengo.Node(lambda t: env.sample_gamma(t))
         in_phase = nengo.Node(lambda t: env.sample_phase(t))
-        in_unchosen = nengo.Node(lambda t: env.sample_unchosen(t))
-        in_v_chosen = nengo.Node(lambda t: env.sample_v_chosen(t))
         target_omega = nengo.Node(lambda t: env.sample_omega(t))
         stop_load = nengo.Node(lambda t: 1 if t>env.t_load else 0)
         diff_output = nengo.Node(size_in=1)
@@ -166,7 +156,8 @@ def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
         load = nengo.Ensemble(n_neurons, 1)
         relax = nengo.Ensemble(n_neurons, 1)
         diff = nengo.Ensemble(n_neurons, 3, radius=2)
-        delta_rel = nengo.Ensemble(n_neurons, 2)
+        delta_rel = nengo.Ensemble(n_neurons, 6)
+        v_chosen = nengo.Ensemble(n_neurons, 2)
         
         nengo.Connection(in_letter, letter, synapse=None)
         nengo.Connection(in_location, location, synapse=None)
@@ -185,15 +176,22 @@ def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
         
         nengo.Connection(omega, omega, synapse=0.1)
 
-        # update omega by Delta_Rel
-        nengo.Connection(in_v_chosen, delta_rel)
-        nengo.Connection(delta_rel, diff[0], function=lambda x: np.abs(x[0]-x[1]))
-        nengo.Connection(delta_rel, diff[1], function=lambda x: 1 if x[0]>x[1] else 0)
+        nengo.Connection(in_action[0], delta_rel[0])
+        nengo.Connection(in_action[1], delta_rel[1])
+        nengo.Connection(value_letter[0], delta_rel[2])
+        nengo.Connection(value_letter[1], delta_rel[3])
+        nengo.Connection(value_location[0], delta_rel[4])
+        nengo.Connection(value_location[1], delta_rel[5])
+
+        nengo.Connection(delta_rel, v_chosen[0], function=lambda x: x[0]*x[2]+x[1]*x[3])
+        nengo.Connection(delta_rel, v_chosen[1], function=lambda x: x[0]*x[4]+x[1]*x[5])
+        nengo.Connection(v_chosen, diff[0], function=lambda x: np.abs(x[0]-x[1]))
+        nengo.Connection(v_chosen, diff[1], function=lambda x: 1 if x[0]>x[1] else 0)
         nengo.Connection(omega, diff[2])
-        nengo.Connection(diff, omega, function=lambda x: 0.05*x[0]*(x[1]-x[2]))
+        nengo.Connection(diff, omega, function=lambda x: 0.1*x[0]*(x[1]-x[2]))
         nengo.Connection(diff, diff_output, function=lambda x: x[0]*(x[1]-x[2]))
-        nengo.Connection(in_phase[0], diff.neurons, transform=inh_feedback)
-        
+        nengo.Connection(in_phase[0], diff.neurons, transform=inh_feedback)        
+
         c0 = nengo.Connection(letter[0], value_letter[0], transform=0, learning_rule_type=pes)
         c1 = nengo.Connection(letter[1], value_letter[1], transform=0, learning_rule_type=pes)
         c2 = nengo.Connection(location[0], value_location[0], transform=0, learning_rule_type=pes)
@@ -252,6 +250,7 @@ def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
         net.p_gamma = nengo.Probe(gamma)
         net.p_error = nengo.Probe(error)
         net.p_decay = nengo.Probe(decay)
+        net.p_v_chosen = nengo.Probe(v_chosen)
         net.p_delta_rel = nengo.Probe(delta_rel)
         net.p_diff = nengo.Probe(diff)
         net.p_diff_output = nengo.Probe(diff_output)
@@ -269,22 +268,17 @@ def simulate_network(net, blocks=24):
     with sim:
         sim.run(net.env.t_load)
         for bid in env.empirical.query("sid==@sid")['bid'].unique()[:blocks]:
-            # env.set_reward_schedule()
             for trial in env.empirical.query("sid==@sid & bid==@bid")['trial'].unique():
                 print(f"running sid {env.sid}, block {bid}, trial {trial}")
                 env.set_cue(bid, trial)
                 sim.run(env.t_cue)
                 env.set_action(sim, net)
-                env.set_v_chosen(sim, net)
                 env.set_omega(sim, net)
                 env.set_reward(bid, trial)
                 sim.run(env.t_reward)
-                # did they choose the better option?
                 block = env.empirical.query("sid==@sid & bid==@bid & trial==@trial")['block'].to_numpy()[0]
                 correct = env.empirical.query("sid==@sid & bid==@bid & trial==@trial")['correct'].to_numpy()[0]
-                if env.action==[1,0] and correct=='left':
-                    accuracy = 1
-                elif env.action==[0,1] and correct=='right':
+                if (env.action==[1,0] and correct=='left') or (env.action==[0,1] and correct=='right'):
                     accuracy = 1
                 else:
                     accuracy = 0
@@ -295,12 +289,10 @@ def simulate_network(net, blocks=24):
     data = pd.concat(dfs, ignore_index=True)
     return sim, data
 
-
-sid = int(sys.argv[1])
-
-env = Environment(sid=sid)
-net = build_network(env, seed_network=sid)
-sim, data = simulate_network(net)
-
-data.to_pickle(f"data/model0_sid{sid}_behavior.pkl")
-print(data)
+if __name__ == "__main__":
+	sid = int(sys.argv[1])
+	env = Environment(sid=sid)
+	net = build_network(env, seed_network=sid)
+	sim, data = simulate_network(net)
+	data.to_pickle(f"data/model0_sid{sid}_behavior.pkl")
+	print(data)
