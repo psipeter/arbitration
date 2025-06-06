f4a956a3f93283d5bd5d4e97b764c8527a2610ff
Peter Duggins <psipeter@hydra.dartmouth.edu>
2025-06-06 15:59:15 -0400

    update plotting code to correctly display letter value

--- model_0.py
+++ model_0.py
@@ -8,11 +8,14 @@ import sys
 
 class Environment():
     def __init__(self, sid, seed=1, t_load=1, t_cue=0.5, t_reward=0.5, dt=0.001,
-                 alpha_plus=0.47, alpha_minus=0.44, gamma_u=0.32,
-                 omega_0=0.35, alpha_omega=0.31, gamma_omega=0.09, p_reward=0.7):
+    			 noise_freq=10, noise_rms=0.05, p_reward=1,
+                 alpha_plus=0.5, alpha_minus=0.5, gamma_u=0.32,
+                 omega_0=0.35, alpha_omega=0.31, gamma_omega=0.09):
         self.empirical = pd.read_pickle("data/empirical.pkl")
         self.sid = sid
         self.rng = np.random.RandomState(seed=seed)
+        self.white_signal_left = nengo.processes.WhiteSignal(period=1000, high=noise_freq, rms=noise_rms, seed=seed)
+        self.white_signal_right = nengo.processes.WhiteSignal(period=1000, high=noise_freq, rms=noise_rms, seed=seed+1)
         self.t_load = t_load
         self.t_cue = t_cue
         self.t_reward = t_reward
@@ -32,7 +35,8 @@ class Environment():
         self.gamma = [0,0]
         self.action = [0,0]
         self.feedback_phase = 0
-        self.cue_phase = 1
+        self.cue_phase = 0
+        self.load_phase = 1
     def set_cue(self, bid, trial):
         sid = self.sid
         left = self.empirical.query("sid==@sid & bid==@bid & trial==@trial")['left'].to_numpy()[0]
@@ -42,12 +46,13 @@ class Environment():
         self.reward = [0,0]
         self.alpha = [0,0]
         self.gamma = [0,0]
-        self.action = [1,1]  # prevents learning during cue phase
+        self.action = [0,0]
         self.cue_phase = 1
         self.feedback_phase = 0
+        self.load_phase = 0
     def set_action(self, sim, net):
-        act = sim.data[net.p_action][-1]
-        self.action = [1, 0] if act[0]>act[1] else [0,1]
+        # self.action = [1, 0] if sim.data[net.p_action_left][-1]>sim.data[net.p_action_right][-1] else [0,1]
+        self.action = [1, 0] if sim.data[net.p_value_left][-1]>sim.data[net.p_value_right][-1] else [0,1]
     def set_reward(self, bid, trial):
         sid = self.sid
         block = self.empirical.query("sid==@sid & bid==@bid & trial==@trial")['block'].to_numpy()[0]
@@ -92,6 +97,7 @@ class Environment():
                     self.gamma = [self.gamma_u,0]
         self.cue_phase = 0
         self.feedback_phase = 1
+        self.load_phase = 0
     def set_omega(self, sim, net):
         chosen = 0 if self.action==[1,0] else 1
         v_c_letter = sim.data[net.p_value_letter][-1,chosen]
@@ -107,7 +113,9 @@ class Environment():
         return self.location
     def sample_action(self, t):
         return self.action
-    def sample_omega(self, t):
+    def sample_omega_0(self, t):
+        return self.omega_0
+    def sample_target_omega(self, t):
         return self.omega
     def sample_reward(self, t):
         return self.reward
@@ -116,29 +124,28 @@ class Environment():
     def sample_gamma(self, t):
         return self.gamma
     def sample_phase(self, t):
-        return [self.cue_phase, self.feedback_phase]
+        return [self.cue_phase, self.feedback_phase, self.load_phase]
 
-def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
+def build_network(env, n_neurons=2000, seed_network=1, inh=0, k=1.0, a=4e-5):
     net = nengo.Network(seed=seed_network)
     net.env = env
     net.config[nengo.Connection].synapse = 0.01
     net.config[nengo.Probe].synapse = 0.1
-    w_fb = np.array([[1, inh], [inh, 1]])
     inh_feedback = -1000*np.ones((n_neurons, 1))
     pes = nengo.PES(learning_rate=a)
 
     with net:
         in_letter = nengo.Node(lambda t: env.sample_letter(t))
         in_location = nengo.Node(lambda t: env.sample_location(t))
-        in_omega = nengo.Node(lambda t: env.omega_0)
+        in_omega = nengo.Node(lambda t: env.sample_omega_0(t))
         in_action = nengo.Node(lambda t: env.sample_action(t))
         in_reward = nengo.Node(lambda t: env.sample_reward(t))
         in_alpha = nengo.Node(lambda t: env.sample_alpha(t))
         in_gamma = nengo.Node(lambda t: env.sample_gamma(t))
         in_phase = nengo.Node(lambda t: env.sample_phase(t))
-        target_omega = nengo.Node(lambda t: env.sample_omega(t))
-        stop_load = nengo.Node(lambda t: 1 if t>env.t_load else 0)
-        diff_output = nengo.Node(size_in=1)
+        target_omega = nengo.Node(lambda t: env.sample_target_omega(t))
+        # in_noise_action_left = nengo.Node(env.white_signal_left)
+        # in_noise_action_right = nengo.Node(env.white_signal_right)
         
         letter = nengo.Ensemble(n_neurons, 2, radius=2)
         location = nengo.Ensemble(n_neurons, 2, radius=2)
@@ -148,9 +155,10 @@ def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
         value_left = nengo.Ensemble(n_neurons, 1)
         value_right = nengo.Ensemble(n_neurons, 1)
         reward = nengo.Ensemble(n_neurons, 2)
-        alpha = nengo.Ensemble(n_neurons, 2)
-        gamma = nengo.Ensemble(n_neurons, 2)
-        action = nengo.Ensemble(n_neurons, 2)
+        # alpha = nengo.Ensemble(n_neurons, 2)
+        # gamma = nengo.Ensemble(n_neurons, 2)
+        # action_left = nengo.Ensemble(n_neurons, 1, encoders=nengo.dists.Choice([[1]]), intercepts=nengo.dists.Uniform(0,1))
+        # action_right = nengo.Ensemble(n_neurons, 1, encoders=nengo.dists.Choice([[1]]), intercepts=nengo.dists.Uniform(0,1))
         error = nengo.Ensemble(n_neurons, 4, radius=3)
         decay = nengo.Ensemble(n_neurons, 4, radius=3)
         load = nengo.Ensemble(n_neurons, 1)
@@ -162,8 +170,8 @@ def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
         nengo.Connection(in_letter, letter, synapse=None)
         nengo.Connection(in_location, location, synapse=None)
         nengo.Connection(in_reward, reward, synapse=None)
-        nengo.Connection(in_alpha, alpha, synapse=None)
-        nengo.Connection(in_gamma, gamma, synapse=None)
+        # nengo.Connection(in_alpha, alpha, synapse=None)
+        # nengo.Connection(in_gamma, gamma, synapse=None)
 
         nengo.Connection(in_omega, load)
         nengo.Connection(in_omega, relax)
@@ -171,7 +179,9 @@ def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
         nengo.Connection(relax, omega, transform=env.gamma_omega*0.1)
         nengo.Connection(omega, load, transform=-1)
         nengo.Connection(omega, relax, transform=-1)
-        nengo.Connection(stop_load, load.neurons, transform=inh_feedback)
+        # nengo.Connection(in_phase[2], load.neurons, transform=inh_feedback)
+        nengo.Connection(in_phase[0], load.neurons, transform=inh_feedback)
+        nengo.Connection(in_phase[1], load.neurons, transform=inh_feedback)
         nengo.Connection(in_phase[0], relax.neurons, transform=inh_feedback)
         
         nengo.Connection(omega, omega, synapse=0.1)
@@ -189,7 +199,6 @@ def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
         nengo.Connection(v_chosen, diff[1], function=lambda x: 1 if x[0]>x[1] else 0)
         nengo.Connection(omega, diff[2])
         nengo.Connection(diff, omega, function=lambda x: 0.1*x[0]*(x[1]-x[2]))
-        nengo.Connection(diff, diff_output, function=lambda x: x[0]*(x[1]-x[2]))
         nengo.Connection(in_phase[0], diff.neurons, transform=inh_feedback)        
 
         c0 = nengo.Connection(letter[0], value_letter[0], transform=0, learning_rule_type=pes)
@@ -204,13 +213,23 @@ def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
         nengo.Connection(value_letter, value_right, function=lambda x: x[1]*x[2])
         nengo.Connection(value_location, value_right, function=lambda x: x[1]*(1-x[2]))
         
-        nengo.Connection(value_left, action[0], transform=k)
-        nengo.Connection(value_right, action[1], transform=k)
-        nengo.Connection(action, action, transform=w_fb, synapse=0.1)
+        # nengo.Connection(value_left, action_left, transform=k)
+        # nengo.Connection(value_right, action_right, transform=k)
+        # nengo.Connection(in_noise_action_left, action_left, synapse=None)
+        # nengo.Connection(in_noise_action_right, action_right, synapse=None)
+        # nengo.Connection(action_left, action_left, transform=1, synapse=0.1)
+        # nengo.Connection(action_left, action_right, transform=inh, synapse=0.1)
+        # nengo.Connection(action_right, action_left, transform=inh, synapse=0.1)
+        # nengo.Connection(action_right, action_right, transform=1, synapse=0.1)
 
         nengo.Connection(in_phase[0], error.neurons, transform=inh_feedback, synapse=None)
+        nengo.Connection(in_phase[2], error.neurons, transform=inh_feedback, synapse=None)
         nengo.Connection(in_phase[0], decay.neurons, transform=inh_feedback, synapse=None)
-        nengo.Connection(in_phase[1], action.neurons, transform=inh_feedback, synapse=None)
+        nengo.Connection(in_phase[2], decay.neurons, transform=inh_feedback, synapse=None)
+        # nengo.Connection(in_phase[1], action_left.neurons, transform=inh_feedback, synapse=None)
+        # nengo.Connection(in_phase[1], action_right.neurons, transform=inh_feedback, synapse=None)
+        # nengo.Connection(in_phase[2], action_left.neurons, transform=inh_feedback, synapse=None)
+        # nengo.Connection(in_phase[2], action_right.neurons, transform=inh_feedback, synapse=None)
 
         nengo.Connection(value_letter[0], error[0], transform=-1)
         nengo.Connection(value_letter[1], error[1], transform=-1)
@@ -218,8 +237,8 @@ def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
         nengo.Connection(value_location[1], error[1], transform=-1)
         nengo.Connection(reward[0], error[0], transform=+1)
         nengo.Connection(reward[1], error[1], transform=+1)
-        nengo.Connection(alpha[0], error[2])
-        nengo.Connection(alpha[1], error[3])
+        nengo.Connection(in_alpha[0], error[2])
+        nengo.Connection(in_alpha[1], error[3])
 
         nengo.Connection(error, c0.learning_rule, function=lambda x: -x[0]*x[2])
         nengo.Connection(error, c2.learning_rule, function=lambda x: -x[0]*x[2])
@@ -230,8 +249,8 @@ def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
         nengo.Connection(value_letter[1], decay[1], transform=-1)
         nengo.Connection(value_location[0], decay[0], transform=-1)
         nengo.Connection(value_location[1], decay[1], transform=-1)
-        nengo.Connection(gamma[0], decay[2])
-        nengo.Connection(gamma[1], decay[3])
+        nengo.Connection(in_gamma[0], decay[2])
+        nengo.Connection(in_gamma[1], decay[3])
 
         nengo.Connection(decay, c0.learning_rule, function=lambda x: -x[0]*x[2])
         nengo.Connection(decay, c2.learning_rule, function=lambda x: -x[0]*x[2])
@@ -245,15 +264,15 @@ def build_network(env, n_neurons=1000, seed_network=1, inh=0, k=0.2, a=4e-5):
         net.p_value_left = nengo.Probe(value_left)
         net.p_value_right = nengo.Probe(value_right)
         net.p_reward = nengo.Probe(reward)
-        net.p_action = nengo.Probe(action)
-        net.p_alpha = nengo.Probe(alpha)
-        net.p_gamma = nengo.Probe(gamma)
+        # net.p_action_left = nengo.Probe(action_left)
+        # net.p_action_right = nengo.Probe(action_right)
+        # net.p_alpha = nengo.Probe(alpha)
+        # net.p_gamma = nengo.Probe(gamma)
         net.p_error = nengo.Probe(error)
         net.p_decay = nengo.Probe(decay)
         net.p_v_chosen = nengo.Probe(v_chosen)
         net.p_delta_rel = nengo.Probe(delta_rel)
         net.p_diff = nengo.Probe(diff)
-        net.p_diff_output = nengo.Probe(diff_output)
         net.p_omega = nengo.Probe(omega)
         net.p_omega_target = nengo.Probe(target_omega)
 
