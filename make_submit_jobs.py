2a273465487377d5051193ee69402c1a23bd015b
Peter Duggins <psipeter@hydra.dartmouth.edu>
2025-08-27 11:13:45 -0400

    typo

--- make_submit_jobs.py
+++ make_submit_jobs.py
@@ -6,23 +6,24 @@ import time
 
 sessions = range(3)
 
-for session in sessions:
-	fit_string = f"python model1p2.py {monkey} {session} load fitting"
-	file_string = f'job_s{session}.sh'
-	with open (file_string, 'w') as rsh:
-		rsh.write('''#!/bin/bash''')
-		rsh.write("\n")
-		rsh.write('''#SBATCH --mem=8G''')
-		rsh.write("\n")
-		rsh.write('''#SBATCH --nodes=1''')
-		rsh.write("\n")
-		rsh.write('''#SBATCH --ntasks-per-node=1''')
-		rsh.write("\n")
-		rsh.write('''#SBATCH --time=12:0:0''')
-		rsh.write("\n")
-		rsh.write(fit_string)
+for monkey in ['V', 'W']:
+	for session in sessions:
+		fit_string = f"python model1p2.py {monkey} {session} load fitting"
+		file_string = f'job_{monkey}{session}.sh'
+		with open (file_string, 'w') as rsh:
+			rsh.write('''#!/bin/bash''')
+			rsh.write("\n")
+			rsh.write('''#SBATCH --mem=8G''')
+			rsh.write("\n")
+			rsh.write('''#SBATCH --nodes=1''')
+			rsh.write("\n")
+			rsh.write('''#SBATCH --ntasks-per-node=1''')
+			rsh.write("\n")
+			rsh.write('''#SBATCH --time=12:0:0''')
+			rsh.write("\n")
+			rsh.write(fit_string)
 
-for session in sessions:
-	submit_string = ["sbatch", f"job_s{session}.sh"]
-	a = subprocess.run(submit_string)
-	time.sleep(1)  # wait a few seconds before next submission to help out SLURM system
+	for session in sessions:
+		submit_string = ["sbatch", f"job_s{session}.sh"]
+		a = subprocess.run(submit_string)
+		time.sleep(1)  # wait a few seconds before next submission to help out SLURM system
