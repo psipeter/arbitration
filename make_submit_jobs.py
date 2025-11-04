b5b76bb745d8b2bf1fe6bfe26cc753e3e01960ea
Peter Duggins <psipeter@hydra.dartmouth.edu>
2025-11-04 17:19:16 -0500

    prepare for perturbation experiments; choose parameters randomly, no fitting

--- make_submit_jobs.py
+++ make_submit_jobs.py
@@ -4,26 +4,27 @@ import subprocess
 import numpy as np
 import time
 
-sessions = range(3)
+for monkey in ['V', 'W']:
+	for session in range(3):
+		for block in range(1,25):
+			fit_string = f"python model_1p3.py {monkey} {session} {block}"
+			file_string = f'job_nef_{monkey}_{session}_{block}.sh'
+			with open (file_string, 'w') as rsh:
+				rsh.write('''#!/bin/bash''')
+				rsh.write("\n")
+				rsh.write('''#SBATCH --mem=8G''')
+				rsh.write("\n")
+				rsh.write('''#SBATCH --nodes=1''')
+				rsh.write("\n")
+				rsh.write('''#SBATCH --ntasks-per-node=1''')
+				rsh.write("\n")
+				rsh.write('''#SBATCH --time=1:0:0''')
+				rsh.write("\n")
+				rsh.write(fit_string)
 
 for monkey in ['V', 'W']:
-	for session in sessions:
-		fit_string = f"python model_1p2.py {monkey} {session} load fitting"
-		file_string = f'job_{monkey}{session}.sh'
-		with open (file_string, 'w') as rsh:
-			rsh.write('''#!/bin/bash''')
-			rsh.write("\n")
-			rsh.write('''#SBATCH --mem=32G''')
-			rsh.write("\n")
-			rsh.write('''#SBATCH --nodes=1''')
-			rsh.write("\n")
-			rsh.write('''#SBATCH --ntasks-per-node=1''')
-			rsh.write("\n")
-			rsh.write('''#SBATCH --time=12:0:0''')
-			rsh.write("\n")
-			rsh.write(fit_string)
-
-	for session in sessions:
-		submit_string = ["sbatch", f"job_{monkey}{session}.sh"]
-		a = subprocess.run(submit_string)
-		time.sleep(1)  # wait a few seconds before next submission to help out SLURM system
+	for session in range(3):
+		for block in range(1,25):
+			submit_string = ["sbatch", f"job_nef_{monkey}_{session}_{block}.sh"]
+			a = subprocess.run(submit_string)
+			time.sleep(1)  # wait a few seconds before next submission to help out SLURM system
