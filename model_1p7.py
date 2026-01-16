25f20eacd6c8934bfa87e95fadf4c2a5393dbbb9
Peter Duggins <psipeter@hydra.dartmouth.edu>
2026-01-16 16:28:36 -0500

    more neurons and higher lr_w or alpha_w leads to clearer omega dynamics

--- model_1p7.py
+++ model_1p7.py
@@ -47,7 +47,7 @@ def get_params(seed, monkey, session, block, trials=80, config='fixed'):
         'p_rew':0.7,
         'lr_let':4e-6,
         'lr_loc':2e-6,
-        'lr_w':4e-5,
+        'lr_w':5e-5,
         'ramp':1.0,
         'thr':0.5,
         'w0':0.5,
@@ -67,7 +67,7 @@ def get_params(seed, monkey, session, block, trials=80, config='fixed'):
         params_net = {
             'alpha_v':rng_net.uniform(0.4, 0.6),
             'gamma_v':rng_net.uniform(0.8, 1.0),
-            'alpha_w':rng_net.uniform(0.6, 0.8),
+            'alpha_w':rng_net.uniform(0.7, 0.9),
             # 'w0':rng_net.uniform(0.49, 0.51),
         }
     params = params | params_net  # combine two parameter dictionaries
