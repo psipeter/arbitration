04e6698ed8908f3e45f86629ce29fbd6bd3b0600
Peter Duggins <psipeter@hydra.dartmouth.edu>
2026-02-26 13:18:07 -0500

    do probe flag in compress

--- compress_nef_data.py
+++ compress_nef_data.py
@@ -3,20 +3,10 @@ import re
 import h5py
 from pathlib import Path
 
-def process_nef_data(folder_path="data/nef"):
+def process_nef_data(folder_path="data/nef/pert", do_probe=False):
     path = Path(folder_path)
     output_probes = path / "nef_data_probes.pkl.xz"
     output_values  = path / "nef_data_values.pkl.xz"
-
-    probe_files = list(path.glob("*_probes.pkl"))
-    if probe_files:
-        print(f"Concatenating {len(probe_files)} '_probes.pkl' files...")
-        df_probe = pd.concat((pd.read_pickle(f) for f in probe_files), ignore_index=True)
-        print(f"Compressing (xz) and saving to {output_probes}...")
-        df_probe.to_pickle(output_probes, compression="xz")
-        del df_probe 
-    else:
-        print("No '_probes.pkl' files found.")
 
     value_files = list(path.glob("*_values.pkl"))
     if value_files:
@@ -27,6 +17,17 @@ def process_nef_data(folder_path="data/nef"):
         del df_value 
     else:
         print("No '_values.pkl' files found.")
+
+    if do_probe:
+        probe_files = list(path.glob("*_probes.pkl"))
+        if probe_files:
+            print(f"Concatenating {len(probe_files)} '_probes.pkl' files...")
+            df_probe = pd.concat((pd.read_pickle(f) for f in probe_files), ignore_index=True)
+            print(f"Compressing (xz) and saving to {output_probes}...")
+            df_probe.to_pickle(output_probes, compression="xz")
+            del df_probe 
+        else:
+            print("No '_probes.pkl' files found.")
 
     print("\nProcess complete.")
 
